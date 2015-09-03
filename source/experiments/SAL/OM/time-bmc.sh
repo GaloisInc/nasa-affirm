06c124fe54c7ee2672b5acb1a07d2b89d681d71f
Benjamin Jones <bjones@galois.com>
2015-09-02 21:46:11 -0700

    om1_cal_v4 model is working
    - model now passes all the built-in tests
    - sal-bmc verifies the 'vaa' theorem out to depth 40
    - see `time-bmc-v4.log` for timing data

--- source/experiments/SAL/OM/time-bmc.sh
+++ source/experiments/SAL/OM/time-bmc.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-#PARAMS="1,1 1,2 2,1 2,2 2,3 3,2"
-PARAMS="1,1 2,1 3,1 1,2 2,2 3,2 3,3"
-DEPTH=50
-#SAL=sal-inf-bmc
-SAL=sal-bmc
+MODEL="om1_cal_v4.sal"
+THM="vaa"
+SAL="sal-bmc -v 1 --disable-traceability"
+DEPTHS=$(seq 1 40)
 
-for p in $PARAMS; do
-    echo "------------"
-    echo "(n,k) = ($p)"
-    (time $SAL --disable-traceability -d $DEPTH "om1_cal_v3{$p}" \
-          not_all_done) 2>&1 \
+for d in $DEPTHS; do
+    #echo "$SAL -d $d $MODEL $THM"
+    echo -n "$d  "
+    (time $SAL -d $d $MODEL $THM) 2>&1 \
       | grep real \
       | awk '{print $2}'
 done
