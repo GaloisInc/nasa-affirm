7227a236b745daff530a749fef31391f6b7d9146
Benjamin Jones <bjones@galois.com>
2016-03-31 10:52:07 -0700

    Improve benchmark script output and exit code checking

--- source/experiments/SAL/OM/bench/time_om1.sh
+++ source/experiments/SAL/OM/bench/time_om1.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e -o pipefail
+set -o pipefail
 
 ###  PARAMETERS  ###########################################
 
@@ -12,6 +12,8 @@ mixed_params=(\
     "5,1" "4,2" "3,3" "2,4" "1,5" \
     "6,1" "5,2" "4,3" "3,4" "2,5" "1,6" \
     "7,1" "6,2" "5,3" "4,4" "3,5" "2,6" "1,7" \
+    "8,1" "7,2" "6,3" "5,4" "4,5" "3,6" "2,7" "1,8" \
+    "9,1" "8,2" "7,3" "6,4" "5,5" "4,6" "3,7" "2,8" "1,9"\
 )
 
 small_params=(\
@@ -20,10 +22,17 @@ small_params=(\
     "3,1" "2,2" "1,3" \
 )
 
-diag_params=("1,1" "2,2" "3,3" "4,4" "5,5")
+diag_params=("1,1" "2,2" "3,3" "4,4" "5,5" "6,6" "7,7")
 
 # Specify which parameter set to use
-param_set="${small_params[@]}"
+param_set="${mixed_params[@]}"
+
+# Specify how many concurrent jobs to run
+CONCURRENT_LIMIT=${CONCURRENT_LIMIT:-8}
+export CONCURRENT_LIMIT
+
+# specify timeout
+TIME_LIMIT=${TIME_LIMIT:-"1h"}
 
 
 ###  JOB COMMANDS  #########################################
@@ -39,18 +48,27 @@ SAL_BMC=$(resolve_prog sal-inf-bmc)  # SAL inf-state bounded model checker
 # temporary file, match the "total execution time" line and extract run time
 sal_smc_cmd() {
     local model=$1
+    local par=$2
     local tmp=$(mktemp sal_cmd.XXXXXX -ut)
 
-    ${SAL_SMC} -v 1 \
+    timeout $TIME_LIMIT \
+        ${SAL_SMC} -v 1 \
         --disable-traceability \
         --enable-slicer \
         --enable-dynamic-reorder \
-        "$model" \
-        vaa \
+        "${model}{$par}" vaa \
         2>&1 | tee "$tmp"
 
     # output run time next to job status
-    cat "$tmp" | awk "/total exec/ {print \"proved. time\", \$4, \"s\"}" >&3
+    if [ $? -eq 124 ]; then
+        # timeout
+        echo "RESULT $par, timeout"
+        echo "timeout" >&3
+    else
+        # normal return
+        cat "$tmp" | awk "/total exec/ {print \"RESULT $par,\", \$4}"
+        cat "$tmp" | awk "/total exec/ {print \"proved\", \$4}" >&3
+    fi
     rm "$tmp"
 }
 
@@ -59,14 +77,23 @@ sal_bmc_cmd() {
     local par=$2
     local tmp=$(mktemp sal_cmd.XXXXXX -ut)
 
-    ./runproof.sh $model -par $par \
+    timeout $TIME_LIMIT \
+        ./runproof.sh $model -par "{$par}" \
         2>&1 | tee "$tmp"
     local code=$?
 
     # output run time next to job status
-    cat "$tmp" | awk "/total time ellapsed/ {print \"proved. time\", \$4, \"s\"}" >&3
+    if [ $code -eq 124 ]; then
+        echo "RESULT $par, timeout"
+        echo "timeout" >&3
+    elif [ $code -ne 0 ]; then
+        printf "exit %d" $code
+        printf "exit %d" $code >&3
+    else
+        cat "$tmp" | awk "/total time ellapsed/ {print \"RESULT $par,\", \$4}"
+        cat "$tmp" | awk "/total time ellapsed/ {print \"proved\", \$4}" >&3
+    fi
     rm "$tmp"
-    exit $code
 }
 
 main_rushby() {
@@ -75,7 +102,7 @@ main_rushby() {
 
     for p in "${params[@]}"; do
         jobs=("${jobs[@]}" - "(sal-smc) prove 'vaa' for om1{$p}" \
-              sal_smc_cmd "om1{$p}")
+              sal_smc_cmd "om1" "$p")
     done
 
     concurrent "${jobs[@]}"
@@ -88,13 +115,23 @@ main_affirm() {
     for p in "${params[@]}"; do
         jobs=("${jobs[@]}" - \
               "(sal-inf-bmc) prove 'validity' & 'agreement' for om1_cal{$p}" \
-              sal_bmc_cmd "om1_cal.sal" "{$p}")
+              sal_bmc_cmd "om1_cal.sal" "$p")
     done
 
     concurrent "${jobs[@]}"
 }
 
-# param_set is specified above
-#main_smc "${param_set[@]}"
-main_affirm "${param_set[@]}"
 
+# param_set is specified above
+case $1 in
+    "rushby")
+        main_rushby "${param_set[@]}"
+        ;;
+    "affirm")
+        main_affirm "${param_set[@]}"
+        ;;
+    *)
+        echo "unknown target $1"
+        exit 1
+        ;;
+esac
